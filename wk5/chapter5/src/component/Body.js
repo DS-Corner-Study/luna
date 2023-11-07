494065c0f90343adfa36a133c1b16c769088a268
ssxbin <sxsxbin@gmail.com>
2023-11-07 15:30:55 +0900

    [1106] 6주차 스터디

--- wk5/chapter5/src/component/Body.js
+++ wk5/chapter5/src/component/Body.js
@@ -1,13 +1,28 @@
-import "./Body.css";
+import { useRef, useState } from "react";
 
-function Body({ children }) {
-  console.log(children);
+function Body() {
+  const [text, setText] = useState("");
+  const textRef = useRef();
+
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleOnClick = () => { 
+    if (text.length < 5) {
+      textRef.current.focus(); // 다섯 글자보다 적으면 포커스 실행 & 초기화x
+    } else {
+      alert(text);
+      setText(""); 
+      // 입력 값 초기화 위해 set 함수인 setText 호출 후 인수로 빈 문자열 전달
+    }
+  };
+
   return (
-    <div className="body">
-      <h1>body</h1>
-      <div className="body">{children}</div>
+    <div>
+      <input ref={textRef} value={text} onChange={handleOnChange} />
+      <button onClick={handleOnClick}>작성 완료</button>
     </div>
   );
 }
-
 export default Body;
